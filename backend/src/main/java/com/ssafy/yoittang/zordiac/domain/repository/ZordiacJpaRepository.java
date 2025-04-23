c20b4e0ccec3e8962fdc7d8539ed643185e3486c
KSH0610 <spacer1@naver.com>
2025-04-23 17:17:23 +0900

    [BE] feat: Add zordiac domain, presentation, application
    S12P31A502-163
    
    - implement zordiac bulk insert

---
+++ backend/src/main/java/com/ssafy/yoittang/zordiac/domain/repository/ZordiacJpaRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.yoittang.zordiac.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.yoittang.zordiac.domain.Zordiac;
+
+public interface ZordiacJpaRepository extends JpaRepository<Zordiac, Long> {
+}
