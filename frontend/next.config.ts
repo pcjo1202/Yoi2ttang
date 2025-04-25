80ab688e72b73e07c0ff92687af670868634641f
박창조 <ckdwhd1202@gmail.com>
2025-04-25 17:19:03 +0900

    [FE] chore: update next.config.ts
    S12P31A502-112
    
    - proxy setting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  rewrites: async () => [
+    {
+      source: "/api/:slug*",
+      destination: "https://yoi2ttang.com/api/:slug*",
+    },
+  ],
+}
 
-export default nextConfig;
+export default nextConfig
