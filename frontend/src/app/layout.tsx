f575859a37d09742dec90629a144173dcdd7732c
전종우 <jinlaove17@naver.com>
2025-04-29 15:06:03 +0900

    [FE] feat: Add infinite scroll custom hook
    S12P31A502-307
    
    - Add provider components for query client

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import RootProvider from "@/components/providers/RootProvider"
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "요이땅",
   description: "요이땅",
-};
+}
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <RootProvider> {children}</RootProvider>
       </body>
     </html>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
---
+++ frontend/src/hooks/common/useInfiniteScroll.ts
@@ -0,0 +1,50 @@
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+} from "@tanstack/react-query"
+import { useEffect, useRef } from "react"
+
+interface UseInfiniteScrollProps<TQueryFnData>
+  extends UseInfiniteQueryOptions<TQueryFnData> {
+  rootMargin?: string
+  threshold?: number
+}
+
+const useInfiniteScroll = <TQueryFnData>(
+  data: UseInfiniteScrollProps<TQueryFnData>,
+) => {
+  const {
+    rootMargin = "0px",
+    threshold = 0.25,
+    enabled = true,
+    ...params
+  } = data
+  const targetRef = useRef<HTMLDivElement>(null)
+  const response = useInfiniteQuery({
+    enabled,
+    ...params,
+  })
+
+  useEffect(() => {
+    if (!targetRef.current || !enabled) {
+      return
+    }
+
+    const option = {
+      rootMargin,
+      threshold,
+    }
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && response.hasNextPage) {
+        response.fetchNextPage()
+      }
+    }, option)
+    observer.observe(targetRef.current)
+
+    return () => observer.disconnect()
+  }, [enabled, response.hasNextPage])
+
+  return { targetRef, ...response }
+}
+
+export default useInfiniteScroll
