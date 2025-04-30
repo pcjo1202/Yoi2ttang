96134ffa66ad5983654582a9a78f8ba02b835a06
윤서희 <1015alicia@naver.com>
2025-04-30 14:57:40 +0900

    [FE] refactor: Modify children props in Badge component to mandatory
    S12P21A502-318

--- frontend/src/components/common/Badge.tsx
+++ frontend/src/components/common/Badge.tsx
@@ -1,13 +1,13 @@
 interface BadgeProps {
   className?: string
-  children?: React.ReactNode
+  children: React.ReactNode
 }
 
 const Badge = ({ className = "", children }: BadgeProps) => {
   return (
     <div
       className={`${className} flex items-center justify-center rounded-full py-0.5 px-1 w-20 h-6 text-xs text-white`}>
-      {children ?? <span>기본 뱃지</span>}
+      {children}
     </div>
   )
 }
