5ca3dff6f21bbf1bf833ff8feb83405fabc11345
전종우 <jinlaove17@naver.com>
2025-04-30 11:07:55 +0900

    [FE] design: Modify login ui

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,39 +4,38 @@ import Image from "next/image"
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col justify-center px-6 pb-6 h-dvh">
-      <div className="flex flex-col flex-1 justify-evenly">
+    <div className="flex h-dvh flex-col justify-center px-6 pb-6">
+      <div className="flex flex-1 flex-col justify-evenly">
         <p className="text-title-lg">
           뛰어서 땅따먹기
           <br />
           너의 <span className="text-yoi-500">간지</span>를 보여줘!
         </p>
 
-        <div className="flex justify-end">
+        <div className="flex justify-center">
           <Image
             src="/images/logo.svg"
             alt="logo"
-            width={184}
-            height={136}
-            className="opacity-20"
+            width={46}
+            height={34}
+            className="w-56"
           />
-          <Image src="/images/logo.svg" alt="logo" width={184} height={136} />
         </div>
       </div>
 
       <div className="flex flex-col gap-16">
         <div className="flex flex-col gap-2">
-          <p className="text-sm text-center text-neutral-400">
+          <p className="text-center text-sm text-neutral-400">
             SNS 계정으로 간편하게 로그인 하세요
           </p>
 
-          <Button className="flex items-center gap-4 bg-kakao-yellow">
+          <Button className="bg-kakao-yellow flex items-center gap-4">
             <KakaoIcon />
-            <p className="text-lg text-kakao-brown">카카오로 달려들기</p>
+            <p className="text-kakao-brown text-lg">카카오로 달려들기</p>
           </Button>
         </div>
 
-        <p className="text-center text-caption text-neutral-200">
+        <p className="text-caption text-center text-neutral-200">
           ⓒ 2025. Yoittang All rights reserved
         </p>
       </div>
