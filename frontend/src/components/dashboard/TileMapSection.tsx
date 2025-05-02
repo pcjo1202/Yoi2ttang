d86ffdc26d69d0b7f5200eeee2c93d3780df1d89
박창조 <ckdwhd1202@gmail.com>
2025-05-02 14:05:11 +0900

    [FE] feat: Add TileMap Section
    S12P31A502-256
    S12P31A502-240

---
+++ frontend/src/components/dashboard/TileMapSection.tsx
@@ -0,0 +1,26 @@
+import Section from "@/components/common/Section"
+import { ChevronRight } from "lucide-react"
+import Link from "next/link"
+import type { FC } from "react"
+
+interface TileMapSectionProps {}
+
+const TileMapSection: FC<TileMapSectionProps> = ({}) => {
+  return (
+    <Section
+      title="🪵 타일 한눈에 보기"
+      supplement={
+        <Link
+          href="/dashboard/my/tiles"
+          className="text-caption flex items-center gap-1 text-neutral-400">
+          <span>전체 보기</span>
+          <ChevronRight className="size-4" />
+        </Link>
+      }>
+      <div
+        id="map"
+        className="aspect-video w-full rounded-md bg-neutral-200"></div>
+    </Section>
+  )
+}
+export default TileMapSection
