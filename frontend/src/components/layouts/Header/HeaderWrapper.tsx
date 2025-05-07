8e28eed4e174f9f396d97209cf628dd2f4a7400b
전종우 <jinlaove17@naver.com>
2025-05-07 15:27:05 +0900

    [FE] design: Modify stack header component style

--- frontend/src/components/layouts/Header/HeaderWrapper.tsx
+++ frontend/src/components/layouts/Header/HeaderWrapper.tsx
@@ -7,7 +7,7 @@ interface HeaderWrapperProps {
 const HeaderWrapper = ({ children }: HeaderWrapperProps) => {
   return (
     <header className="max-w-yoi-width max-h-yoi-header-height sticky top-0 w-full bg-neutral-50">
-      <div className="flex h-full w-full items-center px-5">{children}</div>
+      <div className="flex h-full w-full items-center">{children}</div>
     </header>
   )
 }
