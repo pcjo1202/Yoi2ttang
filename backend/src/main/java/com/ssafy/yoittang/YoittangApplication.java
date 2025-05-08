4fcaafe035a679993ec771de5d7bd7847ea815d4
이주현 <cmh1199@naver.com>
2025-05-08 11:00:44 +0900

    [BE] feat: Add logs git commit hash when server start
    S12P31A502-391

--- backend/src/main/java/com/ssafy/yoittang/YoittangApplication.java
+++ backend/src/main/java/com/ssafy/yoittang/YoittangApplication.java
@@ -1,5 +1,11 @@
 package com.ssafy.yoittang;
 
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +16,18 @@ public class YoittangApplication {
 		SpringApplication.run(YoittangApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logGitCommitHash() {
+		try (InputStream manifestStream = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF")) {
+			if (manifestStream != null) {
+				Manifest manifest = new Manifest(manifestStream);
+				Attributes attr = manifest.getMainAttributes();
+				String version = attr.getValue("Implementation-Version");
+				String buildTime = attr.getValue("Built-Time");
+				System.out.println("🛠️ Build Info - Git: " + version + ", Time: " + buildTime);
+			}
+		} catch (Exception e) {
+			System.err.println("⚠️ Could not read build info: " + e.getMessage());
+		}
+	}
 }
