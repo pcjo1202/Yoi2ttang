cc1573edd6d39a7625d7d3a0844ba5e1fcc488b6
이남재 <dlskawo0409@naver.com>
2025-05-08 16:11:15 +0900

    [BE] feat: Add SwaggerDoc getTile(zordiacId ,lat, lng)
    S12P31A502-374

--- backend/src/main/java/com/ssafy/yoittang/tile/presentation/TileControllerSwaggerDoc.java
+++ backend/src/main/java/com/ssafy/yoittang/tile/presentation/TileControllerSwaggerDoc.java
@@ -1,16 +1,16 @@
 package com.ssafy.yoittang.tile.presentation;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssafy.yoittang.tile.domain.response.TileGetResponseWrapper;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
 
 public interface TileControllerSwaggerDoc {
 
@@ -23,6 +23,20 @@ public interface TileControllerSwaggerDoc {
             )
     })
     ResponseEntity<TileGetResponseWrapper> getTile(
+            @RequestParam Double lat,
+            @RequestParam Double lng
+    );
+
+    @Operation(summary = "특정 점령 지도 확인", description = "특정 팀이 차지한 좌표 근처에 대한 타일의 정보를 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공하면 타일 return",
+                    content = @Content(schema = @Schema(implementation = TileGetResponseWrapper.class))
+            )
+    })
+    ResponseEntity<TileGetResponseWrapper> getTile(
+            @PathVariable(name = "zordiacId") Long zordiacId,
             @RequestParam Double lat,
             @RequestParam Double lng
     );
