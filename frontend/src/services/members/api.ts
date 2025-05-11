45348747526408f58ca1a909f6635e9bf1b7c98e
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-12 00:11:59 +0900

    [FE] feat: Add profile page api considering server component

---
+++ frontend/src/services/members/api.ts
@@ -0,0 +1,68 @@
+import apiClient from "@/lib/http-common"
+import {
+  MemberAutocompleteRequest,
+  MemberAutocompleteResponse,
+  MemberSearchRequest,
+  MembersResponse,
+  ProfileForEditRequest,
+  ProfileForEditResponse,
+} from "@/types/member"
+
+// 프로필 조회
+export const getProfile = async (nickname: string) => {
+  return await fetch(`member?nickname=${nickname}`)
+}
+
+// 팔로워 조회
+export const getFollowers = async (
+  targetId: number,
+): Promise<MembersResponse> => {
+  return await apiClient.get(`/member/${targetId}/followers`)
+}
+
+// 팔로잉 조회
+export const getFollowings = async (
+  targetId: number,
+): Promise<MembersResponse> => {
+  return await apiClient.get(`/member/${targetId}/followings`)
+}
+
+// 팔로잉
+export const postFollow = async (targetId: number) => {
+  return await apiClient.post(`/member/${targetId}/follow`)
+}
+
+// 언팔로잉
+export const deleteFollow = async (targetId: number) => {
+  return await apiClient.delete(`/member/${targetId}/unfollow`)
+}
+
+// 키워드에 맞는 유저 닉네임 조회
+export const getUserNicknames = async ({
+  keyword,
+  pageToken,
+}: MemberAutocompleteRequest): Promise<MemberAutocompleteResponse> => {
+  return await apiClient.get(
+    `/member/autocomplete?keyword=${keyword}&pageToken=${pageToken}`,
+  )
+}
+
+// 키워드에 맞는 유저 조회
+export const getUsers = async ({
+  keyword,
+  pageToken,
+}: MemberSearchRequest): Promise<MembersResponse> => {
+  return await apiClient.get(
+    `/member/search?keyword=${keyword}&pageToken=${pageToken}`,
+  )
+}
+
+// 프로필 수정 페이지 진입 시 프로필 조회
+export const getProfileForEdit = async (): Promise<ProfileForEditResponse> => {
+  return await apiClient.get("/member/edit-profile")
+}
+
+// 프로필 수정
+export const updateProfile = async (data: ProfileForEditRequest) => {
+  return await apiClient.patch("/member/update-profile", data)
+}
---
+++ frontend/src/types/member.ts
@@ -0,0 +1,85 @@
+export type Profile = {
+  memberId: number
+  nickname: string
+  profileImageUrl: string
+  zordiacName: string
+  stateMessage: string
+  followingCount: number
+  followerCount: number
+  isFollow: boolean
+  time: {
+    hour: number
+    minute: number
+    seconds: number
+  }
+  totalDistance: number
+  totalTileCount: number
+  courses: [
+    {
+      courseId: number
+      courseName: string
+      distance: number
+      courseImageUrl: string
+    },
+  ]
+}
+
+export type ProfileForEdit = {
+  memberId: number
+  nickname: string
+  profileImageUrl: string
+  birthdate: string
+  stateMessage: string
+  disclosureStatus: string
+  gender: string
+  weight: number
+}
+
+export type MemberPreview = {
+  memberId: number
+  nickname: string
+  profileImageUrl: string
+  zordiacName: string
+  isFollow: boolean
+}
+
+export type MemberAutocomplete = {
+  memberId: number
+  nickname: string
+}
+
+// request type
+export type MemberAutocompleteRequest = {
+  keyword: string
+  pageToken: number
+}
+
+export type MemberSearchRequest = {
+  keyword: string
+  pageToken: number
+}
+
+export type ProfileForEditRequest = {
+  profileImageFile: File | null
+  nickname: string
+  weight: number
+  stateMessage: string
+  disclosureStatus: "ALL" | "ONLY_ME"
+}
+
+// response type
+export type ProfileResponse = Profile
+
+export type MembersResponse = {
+  data: MemberPreview[]
+  hasNext: boolean
+  pageToken: string
+}
+
+export type ProfileForEditResponse = ProfileForEdit
+
+export type MemberAutocompleteResponse = {
+  data: MemberAutocomplete[]
+  hasNext: boolean
+  pageToken: string
+}
