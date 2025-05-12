c74bc23f2bdb80a9c8cfed49c5b656c1a5c2a030
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-12 22:32:32 +0900

    [FE] feat: Add navigation bar profile button image

--- frontend/src/components/layouts/navigation-bar/NavigationBar.tsx
+++ frontend/src/components/layouts/navigation-bar/NavigationBar.tsx
@@ -2,32 +2,29 @@ import PeopleIcon from "@/assets/icons/navigation-bar/people-icon.svg"
 import PersonIcon from "@/assets/icons/navigation-bar/person-icon.svg"
 import RouteIcon from "@/assets/icons/navigation-bar/route-icon.svg"
 import NavigationButton from "./NavigationButton"
-import RunningButton from "./RunningButton"
+import NavigationRunningButton from "./NavigationRunningButton"
+import NavigationProfileButton from "./NavigationProfileButton"
 
 const NavigationBar = () => {
   return (
-    <div className="fixed bottom-0 w-full font-semibold bg-white border-t h-yoi-navbar-height border-neutral-200 max-w-yoi-width">
-      <div className="absolute flex items-center justify-around w-full h-full">
-        <NavigationButton
-          href="/dashboard/my"
-          icon={<PersonIcon />}
-          label="마이"
-        />
-        <NavigationButton
-          href="/dashboard/team"
-          icon={<PeopleIcon />}
-          label="팀"
-        />
-        <RunningButton />
-        <NavigationButton href="/quest" icon={<RouteIcon />} label="퀘스트" />
-        <NavigationButton
-          href="/profile"
-          icon={
-            <div className="border rounded-full border-neutral-300 size-7" />
-          }
-          label="프로필"
-        />
-      </div>
+    <div className="h-yoi-navbar-height max-w-yoi-width fixed bottom-0 z-9999 flex w-full items-center justify-around border-t border-neutral-200 bg-white font-semibold">
+      <NavigationButton
+        href="/dashboard/my"
+        icon={<PersonIcon className="size-7" />}
+        label="마이"
+      />
+      <NavigationButton
+        href="/dashboard/team"
+        icon={<PeopleIcon className="size-7" />}
+        label="팀"
+      />
+      <NavigationRunningButton />
+      <NavigationButton
+        href="/quest"
+        icon={<RouteIcon className="size-7" />}
+        label="퀘스트"
+      />
+      <NavigationProfileButton href="/profile" label="프로필" />
     </div>
   )
 }
