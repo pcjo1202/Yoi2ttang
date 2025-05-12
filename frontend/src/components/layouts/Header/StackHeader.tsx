4596735c59da191410dc821514697b31adf66f91
전종우 <jinlaove17@naver.com>
2025-05-12 19:24:23 +0900

    [FE] design: Modify stack header style

--- frontend/src/components/layouts/Header/StackHeader.tsx
+++ frontend/src/components/layouts/Header/StackHeader.tsx
@@ -1,23 +1,17 @@
 "use client"
 
 import HeaderWrapper from "@/components/layouts/Header/HeaderWrapper"
-import { cn } from "@/lib/utils"
 import { ChevronLeftIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { ReactNode } from "react"
 
 interface StackHeaderProps {
-  align?: "left" | "center"
   title: string
-  description?: string
+  supplement?: string | ReactNode
   onClick?: () => void
 }
 
-const StackHeader = ({
-  title,
-  align = "center",
-  description,
-  onClick,
-}: StackHeaderProps) => {
+const StackHeader = ({ title, supplement, onClick }: StackHeaderProps) => {
   const router = useRouter()
 
   const handleClick = () => {
@@ -26,25 +20,14 @@ const StackHeader = ({
 
   return (
     <HeaderWrapper>
-      <div className="flex h-full w-full items-center justify-between">
-        <div
-          className={cn(
-            `flex w-full items-center gap-4`,
-            align === "left" && "justify-start",
-            align === "center" && "justify-between",
-          )}>
-          <button
-            className="flex h-full cursor-pointer items-center justify-center"
-            onClick={handleClick}>
-            <ChevronLeftIcon className="size-7" />
-          </button>
-          <div className="text-title-sm">{title}</div>
-          <div></div>
-        </div>
-        {/*  */}
-        <div className="flex basis-1/3 items-center justify-end text-neutral-400">
-          <p className="text-caption">{description}</p>
-        </div>
+      <div className="flex h-full w-full items-center">
+        <button
+          className="flex h-full basis-1/5 cursor-pointer items-center"
+          onClick={handleClick}>
+          <ChevronLeftIcon className="size-7" />
+        </button>
+        <div className="text-title-sm flex-1 text-center">{title}</div>
+        <div className="flex basis-1/5 justify-end">{supplement}</div>
       </div>
     </HeaderWrapper>
   )
