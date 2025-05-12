f6a2193c89e40e91ed5627ef8db2a5dbcf737fdf
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-12 23:01:21 +0900

    [FE] design: Add z-index to stack header component

--- frontend/src/components/layouts/Header/HeaderWrapper.tsx
+++ frontend/src/components/layouts/Header/HeaderWrapper.tsx
@@ -6,7 +6,7 @@ interface HeaderWrapperProps {
 
 const HeaderWrapper = ({ children }: HeaderWrapperProps) => {
   return (
-    <header className="max-w-yoi-width max-h-yoi-header-height h-yoi-header-height sticky top-0 w-full bg-neutral-50">
+    <header className="max-w-yoi-width max-h-yoi-header-height h-yoi-header-height sticky top-0 z-9999 w-full bg-neutral-50">
       <div className="flex h-full w-full items-center px-2">{children}</div>
     </header>
   )
