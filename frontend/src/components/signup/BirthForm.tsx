1978a531ba1a8af6c47b326e2399547e41242902
전종우 <jinlaove17@naver.com>
2025-05-14 13:47:27 +0900

    [FE] fix: Resolve to issue setting birth date after today

--- frontend/src/components/signup/BirthForm.tsx
+++ frontend/src/components/signup/BirthForm.tsx
@@ -56,27 +56,99 @@ const BirthForm = ({ signupData, onChange, onNext }: BirthFormProps) => {
           new Date().getFullYear(),
         ).toString()
 
-        // 년도가 바뀌었을 때, 일자가 최대 일자를 벗어날 경우 보정
+        const year = new Date().getFullYear()
+        const month = new Date().getMonth() + 1
+        const day = new Date().getDate()
+
+        if (validatedBirth.month) {
+          // 현재 년도보다 크거나 같고, 선택한 월이 현재 월보다 크다면 현재 월로 설정
+          if (Number(validatedBirth.year) >= year && number > month) {
+            validatedBirth.month = month.toString()
+          } else {
+            // 그 외의 경우 기존 로직대로 처리
+            validatedBirth.month = clamp(number, 1, 12).toString()
+          }
+        }
+
+        // 월이 바뀌었을 때, 일자가 최대 일자를 벗어날 경우 보정
         if (validatedBirth.day) {
-          validatedBirth.day = Math.min(
-            Number(validatedBirth.day),
-            new Date(
-              Number(validatedBirth.year),
-              Number(validatedBirth.month),
-              0,
-            ).getDate(),
-          ).toString()
+          // 현재 년월과 같고, 일자가 현재 일자보다 크다면 현재 일자로 설정
+          if (
+            Number(validatedBirth.year) >= year &&
+            Number(validatedBirth.month) >= month &&
+            Number(validatedBirth.day) > day
+          ) {
+            validatedBirth.day = day.toString()
+          } else {
+            // 그 외의 경우 해당 월의 최대 일자로 제한
+            validatedBirth.day = Math.min(
+              Number(validatedBirth.day),
+              new Date(
+                Number(validatedBirth.year),
+                Number(validatedBirth.month),
+                0,
+              ).getDate(),
+            ).toString()
+          }
         }
         break
       }
       case "month": {
-        // 월의 범위를 벗어날 경우 최대, 최소값으로 보정
-        validatedBirth.month = clamp(number, 1, 12).toString()
+        const year = new Date().getFullYear()
+        const month = new Date().getMonth() + 1
+
+        // 현재 년도보다 크거나 같고, 선택한 월이 현재 월보다 크다면 현재 월로 설정
+        if (Number(validatedBirth.year) >= year && number > month) {
+          validatedBirth.month = month.toString()
+        } else {
+          // 그 외의 경우 기존 로직대로 처리
+          validatedBirth.month = clamp(number, 1, 12).toString()
+        }
 
         // 월이 바뀌었을 때, 일자가 최대 일자를 벗어날 경우 보정
         if (validatedBirth.day) {
-          validatedBirth.day = Math.min(
-            Number(validatedBirth.day),
+          const year = new Date().getFullYear()
+          const month = new Date().getMonth() + 1
+          const day = new Date().getDate()
+
+          // 현재 년월보다 크거나 같고, 선택한 일이 현재 일보다 크다면 현재 일로 설정
+          if (
+            Number(validatedBirth.year) >= year &&
+            Number(validatedBirth.month) >= month &&
+            Number(validatedBirth.day) > day
+          ) {
+            validatedBirth.day = day.toString()
+          } else {
+            // 그 외의 경우 해당 월의 최대 일자로 제한
+            validatedBirth.day = Math.min(
+              Number(validatedBirth.day),
+              new Date(
+                Number(validatedBirth.year),
+                Number(validatedBirth.month),
+                0,
+              ).getDate(),
+            ).toString()
+          }
+        }
+        break
+      }
+      case "day": {
+        const year = new Date().getFullYear()
+        const month = new Date().getMonth() + 1
+        const day = new Date().getDate()
+
+        // 현재 년월과 같고, 선택한 일자가 현재 일자보다 크다면 현재 일자로 설정
+        if (
+          Number(validatedBirth.year) >= year &&
+          Number(validatedBirth.month) >= month &&
+          number > day
+        ) {
+          validatedBirth.day = day.toString()
+        } else {
+          // 그 외의 경우 기존 로직대로 처리
+          validatedBirth.day = clamp(
+            number,
+            1,
             new Date(
               Number(validatedBirth.year),
               Number(validatedBirth.month),
@@ -84,20 +156,6 @@ const BirthForm = ({ signupData, onChange, onNext }: BirthFormProps) => {
             ).getDate(),
           ).toString()
         }
-        break
-      }
-      case "day": {
-        // 일의 범위를 벗어날 경우 최대, 최소값으로 보정
-        validatedBirth.day = clamp(
-          number,
-          1,
-          new Date(
-            Number(validatedBirth.year),
-            Number(validatedBirth.month),
-            0,
-          ).getDate(),
-        ).toString()
-
         break
       }
     }
