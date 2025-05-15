805ad8899d08da0419332bc1de91a97a5e2efcd3
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-16 01:54:15 +0900

    [FE] refactor: Add select button and autoplay to carousel common component

--- frontend/src/components/common/Carousel.tsx
+++ frontend/src/components/common/Carousel.tsx
@@ -2,19 +2,92 @@
 
 import { cn } from "@/lib/utils"
 import useEmblaCarousel from "embla-carousel-react"
-import { ReactNode } from "react"
+import Autoplay from "embla-carousel-autoplay"
+import { ReactNode, useCallback, useEffect, useState } from "react"
 
 interface CarouselProps {
-  children: ReactNode
+  loop?: boolean
+  autoplay?: boolean
+  autoplayDelay?: number
+  scrollCount?: number // 즉시 이동 버튼이 필요하다면 children의 length 만큼 설정
   className?: string
+  children: ReactNode
 }
 
-const Carousel = ({ children, className }: CarouselProps) => {
-  const [emblaRef] = useEmblaCarousel()
+const Carousel = ({
+  loop = false,
+  autoplay = false,
+  autoplayDelay = 5000,
+  scrollCount = 0,
+  className,
+  children,
+}: CarouselProps) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true },
+    autoplay
+      ? [Autoplay({ delay: autoplayDelay, stopOnInteraction: true })]
+      : [],
+  )
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  // emblaApi의 캐러셀 인덱스를 설정하는 함수
+  const scrollTo = useCallback(
+    (index: number) => {
+      if (!emblaApi) {
+        return
+      }
+
+      emblaApi.scrollTo(index)
+    },
+    [emblaApi],
+  )
+
+  // emblaApi가 가리키는 인덱스를 상태로 set하는 함수
+  const handleSelect = useCallback(() => {
+    if (!emblaApi) {
+      return
+    }
+
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+  }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) {
+      return
+    }
+
+    handleSelect()
+
+    // select(슬라이드가 완전히 정지 되었을 때 발생) 이벤트를 구독하여 슬라이드가 변경될 때마다 handleSelect 함수 호출
+    emblaApi.on("select", handleSelect)
+    return () => {
+      emblaApi.off("select", handleSelect)
+    }
+  }, [emblaApi, handleSelect])
 
   return (
-    <div ref={emblaRef} className={cn("w-full overflow-hidden", className)}>
-      <div className="flex gap-4">{children}</div>
+    <div className="flex flex-col gap-4">
+      <div ref={emblaRef} className={cn("w-full overflow-hidden", className)}>
+        <div className="flex gap-4">{children}</div>
+      </div>
+
+      {scrollCount > 0 && (
+        <div className="flex justify-center gap-4">
+          {Array.from({ length: scrollCount }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => scrollTo(index)}
+              className={cn(
+                "size-3 rounded-full transition-transform ease-in-out",
+                selectedIndex === index
+                  ? "bg-yoi-500 scale-125"
+                  : "bg-neutral-300",
+              )}
+              aria-label={`Go to slide ${index + 1}`}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
