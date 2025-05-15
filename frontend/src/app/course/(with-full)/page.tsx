5d9882afa87ff71645b22d5767ad4b61c6544b9a
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-16 01:55:15 +0900

    [FE] design: Modify course main page ui

--- frontend/src/app/course/(with-full)/page.tsx
+++ frontend/src/app/course/(with-full)/page.tsx
@@ -1,42 +1,69 @@
+"use client"
+
 import PinIcon from "@/assets/icons/course/pin-icon.svg"
 import RouteIcon from "@/assets/icons/navigation-bar/route-icon.svg"
 import Carousel from "@/components/common/Carousel"
 import Section from "@/components/common/Section"
 import QuestCard from "@/components/quest/QuestCard"
+import { Progress } from "@/components/ui/progress"
 import { ChevronRight } from "lucide-react"
 import Link from "next/link"
 
 const CoursePage = () => {
   return (
     <div className="flex flex-col gap-6 p-4">
-      <div className="text-title-md">주어진 길을 신나게 달려보자!</div>
+      <h1 className="text-title-md">길을 따라 달려 볼까요!</h1>
 
       <Section
         title={
-          <h1 className="text-title-sm">
-            <span className="text-yoi-500">타이거</span>님이 진행했던 코스
-          </h1>
+          <div className="text-title-sm w-11/12">
+            <div className="flex">
+              <p className="line-clamp-1 break-all">
+                <span className="text-yoi-500">
+                  WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                </span>
+              </p>
+              <p className="shrink-0">님이</p>
+            </div>
+            <p>진행했던 코스</p>
+          </div>
+        }
+        supplement={
+          <Link
+            href={`/course/bookmark`}
+            className="flex cursor-pointer items-center gap-0.5">
+            <p className="text-caption text-neutral-400">전체 보기</p>
+            <ChevronRight className="size-5 text-neutral-300" />
+          </Link>
         }>
-        <Carousel>
-          {Array.from({ length: 5 }).map((_, index) => (
+        <Carousel
+          loop={true}
+          autoplay={true}
+          autoplayDelay={5000}
+          scrollCount={3}>
+          {Array.from({ length: 3 }).map((_, index) => (
             <div
               key={index}
               className="flex w-full shrink-0 cursor-pointer gap-4 rounded-xl bg-white p-4">
-              <div className="size-24 rounded-xl bg-neutral-200" />
-              <div className="flex flex-1 flex-col justify-between">
+              <div className="size-28 rounded-lg bg-neutral-200" />
+              <div className="flex flex-1 flex-col justify-between py-1">
                 <div>
-                  <h1 className="text-title-sm line-clamp-1 break-all">
+                  <h1 className="line-clamp-1 text-lg break-all">
                     한강 러닝 코스
                   </h1>
-                  <div className="flex items-center gap-1.5 text-neutral-400">
-                    <RouteIcon className="size-3 shrink-0" />
-                    <p className="text-neutral-500">4.3km</p>
-                  </div>
+
+                  <p className="text-neutral-500">4.3km</p>
                 </div>
 
-                <div>
-                  <p className="text-caption">완성도: 70%</p>
-                  <div className="bg-yoi-500 h-3 w-24 rounded-full" />
+                <div className="relative">
+                  <Progress
+                    value={60}
+                    className="bg-neutral-200"
+                    indicatorClassName="bg-yoi-500 rounded-full"
+                  />
+                  <p className="text-caption text-yoi-500 font-medium">
+                    60% 달렸어요!
+                  </p>
                 </div>
               </div>
             </div>
