531048fca94b55cf4eba933b3534e3e6d207a888
윤서희 <1015alicia@naver.com>
2025-05-15 11:16:58 +0900

    [FE] feat: Add useCurrentLocationButton
    S12P31A502-224

--- frontend/src/components/running/RunningStartMap.tsx
+++ frontend/src/components/running/RunningStartMap.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import { useState } from "react"
 import { useMap } from "@/hooks/map/useMap"
 import { useMapMarker } from "@/hooks/map/useMapMarker"
 import { useMapTiles } from "@/hooks/map/useMapTiles"
 import { getOneTeamTileMap } from "@/services/tile/api"
 import { Coordinates } from "@/types/map/navermaps"
 import { Tile } from "@/types/map/tile"
-import { useState } from "react"
 import { getPayload } from "@/lib/auth/util"
+import { useCurrentLocationButton } from "@/hooks/map/useCurrentLocationButton"
+import CurrentLocationBtn from "@/assets/icons/maps/current-location-btn.svg"
 
 interface RunningStartMapProps {
   loc: Coordinates
@@ -20,7 +22,6 @@ const RunningStartMap = ({ loc }: RunningStartMapProps) => {
 
   const handleCenterChange = async (center: Coordinates) => {
     const res = await getOneTeamTileMap(zodiacId, center)
-    console.log("eee")
     setTiles(res.tileGetResponseList)
   }
 
@@ -33,12 +34,20 @@ const RunningStartMap = ({ loc }: RunningStartMapProps) => {
   useMapMarker({ mapRef, loc: loc })
   useMapTiles({ mapRef, tiles })
 
+  const { moveToCurrentLocation } = useCurrentLocationButton({ mapRef })
+
   if (!loc) {
     return null
   }
+
   return (
-    <div className="flex h-full w-full">
+    <div className="relative flex h-full w-full">
       <div id="running-start-map" className="h-full w-full" />
+      <button
+        onClick={moveToCurrentLocation}
+        className="absolute top-2 left-2 z-10 rounded-full bg-white p-2 text-sm shadow-xl">
+        <CurrentLocationBtn className="size-5" />
+      </button>
     </div>
   )
 }
