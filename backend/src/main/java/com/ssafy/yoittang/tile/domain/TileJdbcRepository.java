fbf8ad44cd8da1cdd9d1543204bb8e39aa735cbe
이남재 <dlskawo0409@naver.com>
2025-05-20 10:20:09 +0900

    [BE] refactor: Modify getTileCluster
    S12P31A502-454

--- backend/src/main/java/com/ssafy/yoittang/tile/domain/TileJdbcRepository.java
+++ backend/src/main/java/com/ssafy/yoittang/tile/domain/TileJdbcRepository.java
@@ -2,8 +2,12 @@ package com.ssafy.yoittang.tile.domain;
 
 import java.util.List;
 
+import com.ssafy.yoittang.tile.domain.response.TileClusterGetResponse;
+
 public interface TileJdbcRepository {
     void bulkInsert(List<Tile> tileList);
 
     void updateTileWithTileHistory();
+
+    List<TileClusterGetResponse> getTileCluster(Long zodiacId, String geoHashPrefix);
 }
