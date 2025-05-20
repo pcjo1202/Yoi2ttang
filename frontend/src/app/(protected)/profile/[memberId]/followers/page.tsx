4ec72f66ed913fe1444cb6fd91914697c194959a
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2025-05-21 03:15:27 +0900

    [FE] fix: Modify paramter getting method

--- frontend/src/app/(protected)/profile/[memberId]/followers/page.tsx
+++ frontend/src/app/(protected)/profile/[memberId]/followers/page.tsx
@@ -1,6 +1,8 @@
+import Loading from "@/components/common/Loading"
 import SearchBar from "@/components/common/SearchBar"
 import StackHeader from "@/components/layouts/Header/StackHeader"
 import FollowerList from "@/components/profile/FollowerList"
+import { Suspense } from "react"
 
 const ProfileFollowerPage = () => {
   return (
@@ -9,7 +11,9 @@ const ProfileFollowerPage = () => {
 
       <div className="flex flex-1 flex-col gap-6 p-4">
         <SearchBar placeholder="러너의 이름을 입력해 주세요" />
-        <FollowerList />
+        <Suspense fallback={<Loading />}>
+          <FollowerList />
+        </Suspense>
       </div>
     </div>
   )
