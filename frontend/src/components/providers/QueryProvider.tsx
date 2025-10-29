329287f559a4c0a1a134eab378376044cf4e1f4e
박창조 <ckdwh1202@gmail.com>
2025-10-29 11:07:52 +0900

    [FE] chore: update tanstack query version 5.74.4 -> 5.90.2

--- frontend/src/components/providers/QueryProvider.tsx
+++ frontend/src/components/providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ReactNode } from "react"
 
 interface QueryProviderProps {
@@ -20,7 +21,10 @@ const QueryProvider = (data: QueryProviderProps) => {
   const { children } = data
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
